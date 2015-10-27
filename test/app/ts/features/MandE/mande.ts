630954b4f6ed41c016374f6b4964b5211e5a15b6
Brian Lewis <brian@softwords.com.au>
2015-10-27 18:36:40 +1100

    test - unattended regression testing

---
+++ test/app/ts/features/MandE/mande.ts
@@ -0,0 +1,11 @@
+﻿describe("Hello world", function () {
+  it("says hello", function () {
+    expect("hello Saturn").toEqual("Hello world!");
+  });
+  it("waves goodbye", function () {
+    expect("goodbye Pluto").toEqual("Hello world!");
+  });
+  it("says ta ta ", function () {
+    expect("goodbye Venus").toEqual("Hello world!");
+  });
+});
